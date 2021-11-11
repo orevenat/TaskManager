70be68a8b2031de087fa01c26ebd6017dc16fdbc
Ruslan Knyazev <agrry@yandex.ru>
2021-11-11 23:08:21 +0500

    task state machine added

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,34 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    event :to_dev do
+      transition new_task: :in_development
+    end
+
+    event :back_to_dev do
+      transition %i[in_qa in_code_review] => :in_development
+    end
+
+    event :to_qa do
+      transition new_task: :in_qa
+    end
+
+    event :to_code_reveiw do
+      transition in_qa: :in_code_review
+    end
+
+    event :pre_release do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :release do
+      transition ready_for_release: :released
+    end
+
+    event :archive do
+      transition %i[new_task released] => :archived
+    end
+  end
 end
